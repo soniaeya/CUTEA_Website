c6f4cad507191ba54eeb761b6438413d628fb5a9
Sonia Li <sonialxy00@gmail.com>
2023-03-25 21:35:21 -0400

    set up ci/cd pipelines

--- client/my-app/pages/index.tsx
+++ client/my-app/pages/index.tsx
@@ -9,7 +9,13 @@ import Container from "@mui/material/Container";
 export default function Home() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <ResponsiveAppBar></ResponsiveAppBar>
+
+            <Container style={{ marginTop: '100px' }} maxWidth="lg">
+                <h1>Welcome to CUTEA!</h1>
+                <p>Concordia University Tea Enthusiast Association (CUTEA)</p>
+
+            </Container>
         </div>
     )
 }
